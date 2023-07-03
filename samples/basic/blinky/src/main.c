a60e188733266780434bed9dfa1b98d354b2f780
Karlo Sebalj <karlo.sebalj@greyp.com>
2023-07-03 13:03:37 +0200

    Lesson 1 - Threads

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -4,14 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "zephyr/kernel/thread_stack.h"
+#include "zephyr/sys/printk.h"
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+
+#include <zephyr/drivers/sensor.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+
+#define STACKSIZE 1024
 
 /*
  * A build error on this line means your board is unsupported.
@@ -21,7 +27,9 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
+	printk("main: basic sample started \n");
 	int ret;
+
 
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
