516698e1edbbf9d875d918af89bc3d803d8d11ee
Karlo Sebalj <karlo.sebalj@greyp.com>
2023-07-17 13:01:24 +0200

    Added reading and sending of can messages

--- samples/drivers/can/can_fd_test/src/main.c
+++ samples/drivers/can/can_fd_test/src/main.c
@@ -13,6 +13,9 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/byteorder.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(can_fd);
+
 #define RX_THREAD_STACK_SIZE 512
 #define RX_THREAD_PRIORITY 2
 #define STATE_POLL_THREAD_STACK_SIZE 512
@@ -27,6 +30,9 @@ K_THREAD_STACK_DEFINE(rx_thread_stack, RX_THREAD_STACK_SIZE);
 K_THREAD_STACK_DEFINE(poll_state_stack, STATE_POLL_THREAD_STACK_SIZE);
 
 const struct device *const can_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
+
+struct k_thread rx_thread_data;
+CAN_MSGQ_DEFINE(counter_msgq, 2);
 
 void tx_callback(const struct device *dev, int error, void *user_data)
 {
@@ -57,22 +63,64 @@ int send_function(const struct device *can_dev)
 	return can_send(can_dev, &frame, K_FOREVER, tx_callback, "Sender 1");
 }
 
+
+void rx_thread(void *arg1, void *arg2, void *arg3)
+{
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+	const struct can_filter filter = {
+		.flags = CAN_FILTER_DATA | CAN_FILTER_IDE,
+		.id = COUNTER_MSG_ID,
+		.mask = CAN_EXT_ID_MASK
+	};
+	struct can_frame frame;
+	int filter_id;
+
+	filter_id = can_add_rx_filter_msgq(can_dev, &counter_msgq, &filter);
+	printf("Counter filter id: %d\n", filter_id);
+
+	while (1) {
+		k_msgq_get(&counter_msgq, &frame, K_FOREVER);
+
+		if (frame.dlc != 2U) {
+			printf("Wrong data length: %u\n", frame.dlc);
+			continue;
+		}
+
+		printf("Counter received: %u\n",
+		       sys_be16_to_cpu(UNALIGNED_GET((uint16_t *)&frame.data)));
+	}
+}
+
+void can_rx_callback(const struct device *dev, struct can_frame *frame, void *arg)
+{
+    printf("Received CAN Frame\n");
+    printf("ID: 0x%08X\n", frame->id);
+    printf("DLC: %d\n", frame->dlc);
+    printf("Data: ");
+    for (int i = 0; i < frame->dlc; i++) {
+        printf("%02X ", frame->data[i]);
+    }
+    printf("\n");
+}
+
 int main(void)
 {
 
-	struct can_frame change_led_frame = {
-		.flags = 0,
-		.id = LED_MSG_ID,
-		.dlc = 1
-	};
-	struct can_frame counter_frame = {
-		.flags = CAN_FRAME_IDE,
-		.id = COUNTER_MSG_ID,
-		.dlc = 2
-	};
+	// struct can_frame change_led_frame = {
+	// 	.flags = 0,
+	// 	.id = LED_MSG_ID,
+	// 	.dlc = 1
+	// };
+	// struct can_frame counter_frame = {
+	// 	.flags = CAN_FRAME_IDE,
+	// 	.id = COUNTER_MSG_ID,
+	// 	.dlc = 2
+	// };
 
-	uint8_t toggle = 1;
-	uint16_t counter = 0;
+	// uint8_t toggle = 1;
+	// uint16_t counter = 0;
 
 	int ret;
 
@@ -81,6 +129,7 @@ int main(void)
 		return 0;
 	}
 
+	can_set_bitrate(can_dev, 500000);
 
 	ret = can_start(can_dev);
 	if (ret != 0) {
@@ -88,22 +137,56 @@ int main(void)
 		return 0;
 	}
 
+	struct can_frame frame = {
+        .flags = CAN_FRAME_FDF,
+        .id = COUNTER_MSG_ID,
+        .dlc = 8,
+        .data = {1,2,3,4,5,6,7,8}
+	};
+
+	k_tid_t rx_tid;
+	rx_tid = k_thread_create(&rx_thread_data, rx_thread_stack,
+				 K_THREAD_STACK_SIZEOF(rx_thread_stack),
+				 rx_thread, NULL, NULL, NULL,
+				 RX_THREAD_PRIORITY, 0, K_NO_WAIT);
+	if (!rx_tid) {
+		printf("ERROR spawning rx thread\n");
+	}
+
+	struct can_filter filter = {
+        .id = COUNTER_MSG_ID,
+        .mask = CAN_STD_ID_MASK,
+        .flags = CAN_FILTER_DATA
+    };
+
+    ret = can_add_rx_filter(can_dev, can_rx_callback, NULL, &filter);
+    if (ret < 0) {
+        printf("Failed to add RX filter [%d]", ret);
+        return 0;
+    }
+
 	while (1) {
-		change_led_frame.data[0] = toggle++ & 0x01 ? SET_LED : RESET_LED;
-		/* This sending call is none blocking. */
-		can_send(can_dev, &change_led_frame, K_FOREVER,
-			 NULL,
-			 "LED change");
-		k_sleep(SLEEP_TIME);
+		// change_led_frame.data[0] = toggle++ & 0x01 ? SET_LED : RESET_LED;
+		// /* This sending call is none blocking. */
+		// can_send(can_dev, &change_led_frame, K_FOREVER,
+		// 	 NULL,
+		// 	 "LED change");
+		// k_sleep(SLEEP_TIME);
 
-		UNALIGNED_PUT(sys_cpu_to_be16(counter),
-			      (uint16_t *)&counter_frame.data[0]);
-		counter++;
+		// UNALIGNED_PUT(sys_cpu_to_be16(counter),
+		// 	      (uint16_t *)&counter_frame.data[0]);
+		// counter++;
+
 		/* This sending call is blocking until the message is sent. */
-		can_send(can_dev, &counter_frame, K_MSEC(100), NULL, NULL);
+		// can_send(can_dev, &counter_frame, K_MSEC(100), NULL, NULL);
 
-		k_sleep(SLEEP_TIME);
-		send_function(can_dev);
+		// k_sleep(SLEEP_TIME);
+		// send_function(can_dev);
+
+		ret = can_send(can_dev, &frame, K_MSEC(100), NULL, NULL);
+		if (ret != 0) {
+				LOG_ERR("Sending failed [%d]", ret);
+		}
 
 		k_sleep(SLEEP_TIME);
 	}
