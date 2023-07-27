7cd021ea7cb715ee80aba4763f1a2e30a2b18bcf
Karlo Sebalj <karlo.sebalj@greyp.com>
2023-07-27 13:50:23 +0200

    My solution for thread_start

--- samples/zephyr_rtos_tutorial/my_exercises/threads/thread_start/src/main.c
+++ samples/zephyr_rtos_tutorial/my_exercises/threads/thread_start/src/main.c
@@ -4,10 +4,30 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "zephyr/sys/printk.h"
 #include <zephyr/kernel.h>
 
-int main(void)
-{
-	printk("Hello World! %s\n", CONFIG_BOARD);
-	return 0;
+#define MY_STACK_SIZE 500
+#define MY_PRIORITY 5
+#define SLEEPTIME 500
+
+extern void my_entry_point(void *, void *, void *) {
+	printk("thread_a: thread started \n");
+
+	while (1) {
+		printk("thread_a: thread loop \n");
+		k_msleep(SLEEPTIME);
+	}
+}
+
+K_THREAD_STACK_DEFINE(my_stack_area, MY_STACK_SIZE);
+struct k_thread my_thread_data;
+
+int main(void) {
+  k_thread_create(
+      &my_thread_data, my_stack_area, K_THREAD_STACK_SIZEOF(my_stack_area),
+      my_entry_point, NULL, NULL, NULL, MY_PRIORITY, 0, K_NO_WAIT);
+  k_thread_start(&my_thread_data);
+
+  return 0;
 }
